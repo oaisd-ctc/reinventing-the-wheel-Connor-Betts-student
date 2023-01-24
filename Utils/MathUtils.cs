9ed903b8fba81eb56e8f745585afbd2e1715a85c
Katie Alberda <112493386+Alberda-Katie-Student@users.noreply.github.com>
2023-01-24 13:16:53 +0000

    Math started

--- Utils/MathUtils.cs
+++ Utils/MathUtils.cs
@@ -3,7 +3,84 @@ namespace MyUtilities
 
     public static class MathUtils
     {
+        
+        //Addition
+        public static int Add(int x, int y)
+        {
+            return x+y;
+        }
 
+        public static double Add(double x, double y)
+        {
+            return x+y;
+        }
+
+        public static float Add(float x, float y)
+        {
+            return x+y;
+        }
+
+        //Subtraction
+
+        public static int Subtract(int x, int y)
+        {
+            return x-y;
+        }
+
+        public static double Subtract(double x, double y)
+        {
+            return x-y;
+        }
+        public static float Subtract(float x, float y)
+        {
+            return x-y;
+        }
+
+        //Multiplication
+
+        public static int Multiply(int x, int y)
+        {
+            return x*y;
+        }
+        public static double Multiply(double x, double y)
+        {
+            return x*y;
+        }
+        public static float Multiply(float x, float y)
+        {
+            return x*y;
+        }
+
+        //Division
+        public static int Divide(int x, int y)
+        { 
+            return x/y;
+        }
+        public static double Divide(double x, double y)
+        {
+            return x/y;
+        }
+        public static float Divide(float x, float y)
+        {
+            return x/y;
+        }
+
+        //Square Roots
+        public static int SquareRoot(int x)
+        {
+            return x*x;
+        }
+
+        public static double SquareRoot(double x)
+        {
+            return x*x;
+        }
+        public static float SquareRoot(float x)
+        {
+            return x*x;
+        }
+
+        //Power
 
         
 
